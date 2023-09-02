b66d21234ce3fecc2daf2da02b6f98ffc4e39634
Afiya786 <75537823+Afiya786@users.noreply.github.com>
2023-09-02 10:25:18 +0000

    conversiondata

---
+++ baissic/conversiondata.js
@@ -0,0 +1,35 @@
+// let score = undefined
+// console.log(typeof(score)) /*either you can give the score variable as a method inside psaranthesis inside the typeof
+// or 
+//  you can give it as a variable without any paranthesis:
+// */
+// console.log(typeof score)
+
+// if you want to convert this string to number 
+
+//  let convertnum = Number(score)
+//  console.log(typeof convertnum
+
+/* --------------------------*/
+let no = "afiya"
+let boolval = Boolean(no)
+console.log(boolval)
+/*-------------*/
+
+/*-----
+
+In JavaScript, there's no need to explicitly convert a number to a float,
+ as JavaScript already uses floating-point representation for numbers.
+  When you work with numeric values in JavaScript, 
+  they are typically stored as 64-bit floating-point numbers (often referred to as double-precision floating-point numbers).
+ This means that even if you start with an integer, it's internally represented as a float.
+
+*/
+
+console.log("1" + 2+ 2) /* = 122*/
+console.log(1 + 2 + "2")  /*= 32*/
+
+
+
+
+
