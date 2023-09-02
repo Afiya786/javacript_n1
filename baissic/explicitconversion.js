125d6a9f42e54092de2142051bc6556bb91b0396
Afiya786 <75537823+Afiya786@users.noreply.github.com>
2023-09-02 11:21:52 +0000

    explicitconversion

---
+++ baissic/explicitconversion.js
@@ -0,0 +1,23 @@
+// Explicit Type Conversion (Casting):
+
+/*-------------*/
+// Example 1: String to Number Conversion
+
+const strNumber = "5";     // String
+const num = Number(strNumber);  // Explicitly convert to a Number
+
+console.log(num);  // Output: 5 (a real number, not a string)
+
+// VICE VERSA WITH INTEGER
+
+/* -----------*/
+
+// Boolean Conversion (Truthy and Falsy):
+
+const value1 = "Hello";  // Truthy
+const value2 = 0;        // Falsy
+
+console.log(Boolean(value1));  // Output: true
+console.log(Boolean(value2));  // Output: false
+
+
