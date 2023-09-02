a471a91363c7faef1f27e21618c59ba09a084997
Afiya786 <75537823+Afiya786@users.noreply.github.com>
2023-09-02 01:55:17 +0000

    datatypes

---
+++ baissic/datatypes.js
@@ -0,0 +1,7 @@
+/*we have many types in js : number, bigInt, string, boolean, null, undefined, symbol)
+
+
+the main difference between undefined and null is : sthe typeof undefined iss undefined but the typeof of null is object)*/
+
+console.log(typeof(undefined))
+console.log(typeof(null))
---
+++ baissic/variable.js
@@ -0,0 +1,5 @@
+const name = "afiya"
+let age = "thirteen"
+var address = "bel"
+console.table([name,age,address]);
+/*this is our variable but we prefer not to usse var eyword due to its issuuee*/
